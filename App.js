2184311196ebf5271aa2be4c713ce971112cac9b
Maximo <maxmonteiro86@gmail.com>
2023-09-01 16:47:44 -0300

    cria estilo do formulario

--- App.js
+++ App.js
@@ -16,25 +16,27 @@ export default function App() {
         <Image source={require('./src/assets/logo-m_128.png')} />
       </View>
 
-      <View>
+      <View style={styles.container}>
         <TextInput 
+          style={styles.input}
           placeholder='Email'
           autoCorrect={false}
           onChangeText={() => {}}
         />
 
         <TextInput 
+          style={styles.input}
           placeholder='Senha'
           autoCorrect={false}
           onChangeText={() => {}}
         />
 
-        <TouchableOpacity>
-          <Text>Acessar</Text>
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
-          <Text>Criar conta gratuita</Text>
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar conta gratuita</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
@@ -46,11 +48,51 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#191919"
+    backgroundColor: "#191919",
   },
 
   containerLogo: {
     flex: 1,
     justifyContent: "center",
+  },
+
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: '90%',
+    paddingBottom: 30
+  },
+
+  input: {
+    backgroundColor: '#FFFFFF',
+    width: '90%',
+    marginBottom: 15,
+    padding: 10,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+  },
+
+  btnSubmit: {
+    backgroundColor: "#35aaff",
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+
+  submitText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+
+  btnRegister: {
+    marginTop: 10,
+  },
+
+  registerText: {
+    color: '#fff',
   }
 })
