e70c667128a80e158bb394ee1b46d1cd031064c6
Maximo <maxmonteiro86@gmail.com>
2023-09-01 16:30:35 -0300

    inclui imagem da logo

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  View,
+  KeyboardAvoidingView,
+  Image,
+} from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView>
+      <View>
+        <Image source={require('./src/assets/logo-m_128.png')} />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
