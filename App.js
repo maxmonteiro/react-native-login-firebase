ebc7f4d4b9807a1fa49fc9274269aef37cfdaef0
Maximo <maxmonteiro86@gmail.com>
2023-09-04 21:49:43 -0300

    configura navegacao inicial para Login

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Register" component={Register}/>
       </Stack.Navigator>
